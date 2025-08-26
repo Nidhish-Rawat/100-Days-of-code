3daff0a8e31dd16e8235bc02de30dc29d241630d
Nidhish-Rawat <nidhishrawat02@gmail.com>
2025-08-26 11:13:10 +0530

    Add files via upload

---
+++ Day-1-Code1-(Sum of 2 numbers).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int Num1, Num2, Sum;
+    printf("Enter the first number : ");
+    scanf("%d", &Num1);
+    printf("Enter the second number : ");
+    scanf("%d", &Num2);
+    Sum = Num1 + Num2;
+    printf("Sum of both number : %d \n",Sum);
+    
+}
