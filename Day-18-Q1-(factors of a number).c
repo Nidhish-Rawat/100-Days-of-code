eebc24c204c4eb101fbb4c2f8ae14edcf71e8ba6
Nidhish-Rawat <nidhishrawat02@gmail.com>
2025-11-27 01:25:11 +0530

    Add files via upload

---
+++ Day-18-Q1-(factors of a number).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a Number:");
+	scanf("%d",&num);	
+	int i;
+	for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			printf("%d ",i);
+		}
+	}
+	return 0;
+}
